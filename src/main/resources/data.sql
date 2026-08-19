7fa09fd7dc49b8924b6ed8015f69a31706914c1a
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-08-19 11:10:08 +0900

    chore: seed test members with data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO member(login_id, name, password) VALUES
+('test1', '테스트1', '{bcrypt}$2a$10$M9oV8gZJAauZDoq.63rjbOaN9O8VY.GzhXaDRxJv/24d2ni.rK7Ty'),
+('test2', '테스트2', '{bcrypt}$2a$10$M9oV8gZJAauZDoq.63rjbOaN9O8VY.GzhXaDRxJv/24d2ni.rK7Ty');
